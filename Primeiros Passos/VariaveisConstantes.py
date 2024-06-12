d7118c864bf200ed312e3aacca0a6015d7de21df
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-06-11 22:40:32 -0300

    Variaveis e Constantes

---
+++ Primeiros Passos/VariaveisConstantes.py
@@ -0,0 +1,13 @@
+# VARIÁEIS - mutailidade
+idade, nome = (30, "Pedro")
+print(f"Idade:{idade} \nNome:{nome}\n")
+idade = 18
+print(f"Idade:{idade} \nNome:{nome}\n")
+
+# PYTHON NÃO TEM CONSTANTES, MAS PARA BOAS PRÁTICAS UTILIZASSE EM MAIUSCULO
+
+FALSO = False
+print(FALSO)
+
+# Padrão snake case. Exemplo:
+calcular_os_resultados = 23 + 34
