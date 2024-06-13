ec20e6ec58230d8901e3b7d58dcc4678b8ee433a
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-06-12 22:25:08 -0300

    Convertendo Tipos de Dados

---
+++ Primeiros Passos/ConversaoTipos.py
@@ -0,0 +1,19 @@
+# Como converter tipos de variáveis
+
+# Inteiro para Float
+preco = 10
+print(float(preco))
+
+# Float para inteiro
+saldo = 35.55
+print(int(saldo))
+
+# Numeros para String
+idade = 34
+print(str(idade))
+texto = f"A idade é {idade}"
+print(texto)
+
+# String para números
+valor = "45.5"
+print(float(valor))
