dfcf48ea983d386184544a6785d3c7ff8587f66d
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-06-12 22:24:45 -0300

    Inputs

---
+++ Primeiros Passos/InputOutput.py
@@ -0,0 +1,9 @@
+nome = input("Seu nome: ")
+print(nome)
+
+nome2 = "Gui"
+sobrenome = "Carvalho"
+
+print(nome2, sobrenome, end="...\n")
+print(nome2, sobrenome, sep="@")
+print(nome2, sobrenome, sep="@", end="...\n")
