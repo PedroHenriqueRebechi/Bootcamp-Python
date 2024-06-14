5b61645b2fb9fe91902cdef5606d34cfa718e4fb
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-06-13 22:29:36 -0300

    Introdução à Condicionais

---
+++ 03. Estruturas/Identacao.py
@@ -0,0 +1,11 @@
+num1 = int(input("Numero 1:"))
+num2 = int(input("Numero 2:"))
+
+def igualdade(num1,num2):
+    if num1 == num2:
+        print("Números iguais")
+    else:
+        print("Números diferentes")
+
+
+igualdade(num1,num2)
