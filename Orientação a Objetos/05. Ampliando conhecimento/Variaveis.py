fef1002f80822745405b0025a9aab3057329317f
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-07-09 09:37:42 -0300

    Variáveis de classe e instância

---
+++ Orientação a Objetos/05. Ampliando conhecimento/Variaveis.py
@@ -0,0 +1,28 @@
+"""Todos os objetos nascem com o mesmo número de atributos de classe e de instância.
+Atributos de instância são diferentes para cada objeto (cada objeto tem uma cópia),
+já os atributos de classe são compartilhados entre os objetos.
+"""
+
+class Estudante:
+    escola = "DIO" # Variavel de classe
+
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula # Variável de instancia
+
+    def __str__(self) -> str:
+        return f"{self.nome} - {self.matricula} - {self.escola}"
+
+
+def mostrar_valores(*objs):
+    for obj in objs:
+        print(obj)
+
+
+aluno_1 = Estudante("Guilherme", 1)
+aluno_2 = Estudante("Giovanna", 2)
+mostrar_valores(aluno_1, aluno_2)
+
+Estudante.escola = "Python"
+aluno_3 = Estudante("Chappie", 3)
+mostrar_valores(aluno_1, aluno_2, aluno_3)
