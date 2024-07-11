e30e4680864d3ca68fe0fb01c04412fe370859c3
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-07-11 14:49:17 -0300

    Decoradores

--- Orientação a Objetos/06. Decoradores, Iteradores e Geradores/Decoradores2.py
+++ Orientação a Objetos/06. Decoradores, Iteradores e Geradores/Decoradores2.py
@@ -19,3 +19,37 @@ print(aprender('Python'))
 # Introspecção 
 # Importar functools
 print(aprender.__name__) 
+
+
+# Praticando
+def entrar(func):
+    @functools.wraps(func)
+    def envelope(*args, **kwargs):
+        senha = input("Digite a senha: ")
+        if int(senha) == 1234:
+            print("Logando...")
+            func(*args, **kwargs)
+            print("Saindo...")
+        else:
+            print("Senha errada")
+            return
+        
+    return envelope
+
+@entrar 
+def logado(nome):
+    menu = f"""\n
+    Olá  {nome}
+    ------------------MENU------------------
+    [d] DEPOSITAR
+    [s] SACAR
+    [e] EXTRATO
+    [u] CRIAR USUÁRIO
+    [c] CRIAR CONTA CORRENTE
+    [q] SAIR
+
+    -> """
+
+    print(menu)
+
+logado("Pedro")
