59360cb6cbeac1554bdfc5ea39ffb9557ef844b1
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-07-15 13:38:59 -0300

    Introdução arquivos

---
+++ Orientação a Objetos/08. Manipulação de arquivos/arquivos.py
@@ -0,0 +1,12 @@
+# Modos de abertura: 'r' - leitura | 'w' - gravação | 'a' - anexar
+
+# Para ler
+file = open("Exemplo.txt", "r") 
+
+# Para escrever
+file = open("Exemplo.txt", "w") 
+
+# Para anexar conteúdo a um arquivo existente
+file = open("Exemplo.txt", "a")
+
+file.close()
