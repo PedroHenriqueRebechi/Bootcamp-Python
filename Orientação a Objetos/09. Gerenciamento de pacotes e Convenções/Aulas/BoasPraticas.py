24ce6601bdb9ac183bd042936f59ff9934d17ae7
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-07-16 23:21:24 -0300

    Boas Práticas e PEP 8

---
+++ Orientação a Objetos/09. Gerenciamento de pacotes e Convenções/Aulas/BoasPraticas.py
@@ -0,0 +1,16 @@
+# PEP 8 -> É o guia de estilo para codar em Python, mostra como formatar e usar convenções
+
+# LINK DOCUMENTAÇÂO: https://peps.python.org/pep-0008/
+
+# Principais recomendações do PEP 8: 4 espaços para identação, limitar linhas a 79 caracteres, usar nome de variaveis e funções em snake_case e CamelCase para classes
+
+# Ferramenta de checagem -> flake8 (flake8 "colar caminho")
+# Ferramenta de formatação automatica -> black (black "colar caminho")
+# Ferramenta de organização de importações -> isort (isort "colar caminho")
+
+import email
+import os
+import sys
+
+
+frutas = ["pera", "maça"]
