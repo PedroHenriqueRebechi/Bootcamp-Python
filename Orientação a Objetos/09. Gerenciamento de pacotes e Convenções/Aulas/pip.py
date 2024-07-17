7a8261d8e0b509500c9fda70c6acfa6bd8de0c15
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-07-16 22:39:46 -0300

    Ambientes virtuais e Pip

---
+++ Orientação a Objetos/09. Gerenciamento de pacotes e Convenções/Aulas/pip.py
@@ -0,0 +1,12 @@
+# pip é o gerenciador de pacotes do Python
+
+# Ambientes virtuais - é um ambiente isolado no Python. Sendo assim uma área segura, onde é possível instalar bibliotecas e suas exigências, sem afetar a instalação global do Python ou outros projetos.
+
+# Criar ambiente virtual -> python -m venv "nome"
+# Seleciona pelo VSCode
+
+# Ativando ambiente -> activate "nome"
+# Desativando ambiente -> deactivate "nome"
+
+
+# pipenv
