5f2371da2fe7c354bc8525b63a173780e444e0d4
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-07-26 20:32:28 -0300

    SQL

---
+++ SQL/Introdução e Conceitos Básicos de SQL/introducao.py
@@ -0,0 +1,14 @@
+# DDL - Liguagem de definição de dados
+# DQL - Liguagem de consulta de dados
+# DML - Liguagem de manipulação de dados
+# DTL - Liguagem de trasação de dados
+# ---------------------------------------------
+# MER - Modelo Entidade Relacionamento - representado atráves de Diagramas Entidade Relacionamento (DER)
+
+# Site CreateLy - Cria diagramas
+
+# Entidades - cada uma em seu dominio - "Usuario"
+# Atributos - propriedades das entidades - "Nome", "Email"
+# Relacionamentos - associações entre as entidades 
+
+# Site QuickDataBaseDiagrams - IA que cria modelos de diagramas
