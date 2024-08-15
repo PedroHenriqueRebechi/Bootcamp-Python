ae6acf52c2fdcee1fc4df43ec907a5a35a172e69
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-08-14 21:50:01 -0300

    Inserindo dados

--- Banco de Dados/01. Introdução/conexao.py
+++ Banco de Dados/01. Introdução/conexao.py
@@ -3,4 +3,12 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-conexao = sqlite3.connect(ROOT_PATH / "clientes.db")  # Cria banco de dados SQLite "clientes"
+conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")  # Cria banco de dados SQLite "meu_banco"
+cursor = conexao.cursor()
+
+# cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
+
+nome = "Pedro"
+email = "pedro@gmail.com"
+cursor.execute("INSERT INTO clientes (nome, email) VALUES (?, ?);", (nome, email))
+conexao.commit()
